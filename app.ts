d9690245bed0f17840e2b7fa52e8f95ca369c1bf
shehnila-wahab <160872560+shehnila-wahab@users.noreply.github.com>
2024-05-25 23:10:47 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,43 @@
+let car : string = 'subaru';
+// Test 1: Equality Comparison (True)
+console.log("Is car == 'subaru? I predict True.'");
+console.log(car == 'subaru'); //True
+
+
+// Test 2: strict Equality Comparison (True)
+console.log("Is car === 'subaru? I predict True.'");
+console.log(car === 'subaru'); //True
+
+
+
+// Test 3: InEquality Comparison (false)
+console.log("Is car != 'subaru? I predict false.'");
+console.log(car != 'subaru'); //false
+
+
+
+// Test 4: strict inEquality Comparison (false)
+console.log("Is car !== 'subaru? I predict True.'");
+console.log(car !== 'subaru'); //false
+
+
+// Test 5: lesss than Comparison (false)
+console.log("Is car < 'subaru? I predict false'");
+console.log(car < 'subaru'); //false(lexicograpic comparison)
+
+// Test 6: Greather than Comparison (false)
+console.log("Is car > 'subaru? I predict false.'");
+console.log(car > 'subaru'); //false(lexicograpic comparison)
+
+
+// Test 7: less than or equal Comparison (true)
+console.log("Is car <= 'subaru? I predict true.'");
+console.log(car <= 'subaru'); //true
+
+// Test 8: Greather than or equal Comparison (true)
+console.log("Is car >= 'subaru? I predict true.'");
+console.log(car >= 'subaru'); //true
+
+// Test 9: checking truthiness (true)
+console.log("Is car > 'subaru? I predict true.'");
+console.log(car); //true(non empty string is truely)
